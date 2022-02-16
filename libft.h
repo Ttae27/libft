9d477b8901e35502f57840fc326b897f4fd72661
Pongsakorn Hongpaitoon <phongpai@z3t1c4.42bangkok.com>
2022-02-16 21:08:36 +0700

    a

---
+++ libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phongpai <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 20:59:16 by phongpai          #+#    #+#             */
+/*   Updated: 2022/02/16 21:08:11 by phongpai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int		ft_isalpha(int c);
+
+int		ft_isdigit(int c);
+
+int		ft_isalnum(int c);
+
+int		ft_isascii(int c);
+
+int		ft_isprint(int c);
+
+#endif
